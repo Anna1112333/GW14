7f6f55b3bb13fbe9f7f6bae2dcc242ae6365f9e2
Anna1112333 <anna_1112333@mail.ru>
2022-08-13 13:06:11 +0500

    Task1 cpp

---
+++ 03/Task1/calculator/calculator.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Calculator
+{
+public:
+    double num1;
+    double num2;
+    double add()
+    {
+        return num1 + num2;
+    }
+    double multiply()
+    {
+        return num1 * num2;
+    }
+    double subtract_1_2()
+    {
+        return num1 - num2;
+    }
+    double subtract_2_1()
+    {
+        return num2 - num1;
+    }
+    double divide_1_2()
+    {
+        return num1 / num2;
+    }
+    double divide_2_1()
+    {
+        return num2 / num1;
+    }
+    bool set_num1(double num1)
+    {
+        return static_cast<bool>(num1);
+    }
+    bool set_num2(double num2)
+    {
+        return static_cast<bool>(num2);
+    }
+};
+
+int main()
+{
+    Calculator paar;
+    setlocale(LC_ALL, "rus");
+    do {
+        cout << "Введите num1: ";
+        cin >> paar.num1;
+        cout << "Введите num2: ";
+        cin >> paar.num2;
+        if (paar.set_num1(paar.num1) && paar.set_num2(paar.num2))
+        {
+            cout << "num1 + num2 = " << paar.add() << "\n";
+            cout << "num1 - num2 = " << paar.subtract_1_2() << "\n";
+            cout << "num2 - num1 = " << paar.subtract_2_1() << "\n";
+            cout << "num1 * num2 = " << paar.multiply() << "\n";
+            cout << "num1 / num2 = " << paar.divide_1_2() << "\n";
+            cout << "num2 / num1 = " << paar.divide_2_1() << "\n";
+
+        }
+        else cout << "Неверный ввод!\n";
+    } while (true);
+    return 0;
+
+}
