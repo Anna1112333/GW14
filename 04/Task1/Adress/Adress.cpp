ddc2c14d1ca282c374eb1033481db1379361e19e
Anna1112333 <anna_1112333@mail.ru>
2022-08-23 23:07:10 +0500

    Исправленные 1 и 2 задания 04

--- 04/Task1/Adress/Adress.cpp
+++ 04/Task1/Adress/Adress.cpp
@@ -5,60 +5,61 @@ using namespace std;
 class home
 {
 public:
-    string Sity;
-    string Street;
-    int number_of_house;
-    int number_of_quoter;
+	string Sity;
+	string Street;
+	int number_of_house;
+	int number_of_quoter;
 };
 class function
-{public:
-    int quantity;
-    void setting(ifstream& copy_file_in_class, home* arr1)
-    {
-        for (int i = 0; i < quantity; i++)
-        {
-            copy_file_in_class >> arr1[i].Sity;
-            copy_file_in_class >> arr1[i].Street;
-            copy_file_in_class >> arr1[i].number_of_house;
-            copy_file_in_class >> arr1[i].number_of_quoter;
-        }setting_in_file(arr1);
-    }
-    //Функция вывода класса в файл
+{
+public:
+	int quantity;
+	void setting(ifstream& copy_file_in_class, home* arr1)
+	{
+		for (int i = 0; i < quantity; i++)
+		{
+			copy_file_in_class >> arr1[i].Sity;
+			copy_file_in_class >> arr1[i].Street;
+			copy_file_in_class >> arr1[i].number_of_house;
+			copy_file_in_class >> arr1[i].number_of_quoter;
+		}
+		setting_in_file(arr1);
+	}
+	//Функция вывода класса в файл
 private:
-    void setting_in_file(home* arr1)
-    {
-        ofstream copy_class_out;
-        copy_class_out.open("out.txt");
-        cout << 1234 << arr1[1].Street;
-        for (int i = quantity - 1; i >= 0; i--)
-        {
-            copy_class_out << arr1[i].Sity<<" ";
-            copy_class_out << arr1[i].Street<<" ";
-            copy_class_out << arr1[i].number_of_house<<" ";
-            copy_class_out << arr1[i].number_of_quoter<<" \n";
-        }
-        copy_class_out.close();
-    }
+	void setting_in_file(home* arr1)
+	{
+		ofstream copy_class_out;
+		copy_class_out.open("out.txt");
+		copy_class_out << quantity << "\n";
+		for (int i = quantity - 1; i >= 0; i--)
+		{
+			copy_class_out << arr1[i].Sity << ", ";
+			copy_class_out << arr1[i].Street << ", ";
+			copy_class_out << arr1[i].number_of_house << ", ";
+			copy_class_out << arr1[i].number_of_quoter << ". \n";
+		}
+		copy_class_out.close();
+	}
 };
 
 int main()
 {
-    setlocale(LC_ALL, "Russian"); // На русский
-         
-    ifstream gh("in.txt");
-    if(!gh.is_open())
-    {
-        cout << "Файл in.txt не открывается";
-        return 1;
-    }
-    function func;
-    gh >> func.quantity;
-    home* arr;
-    arr = new home[func.quantity];
+	setlocale(LC_ALL, "Russian"); // На русский
 
-    func.setting( gh, arr);
-    
-    delete[] arr;
-    return 0;
-    gh.close();
-}
+	ifstream gh("in.txt");
+	if (!gh.is_open())
+	{
+		cout << "Файл in.txt не открывается";
+		return 1;
+	}
+	function func;
+	gh >> func.quantity;
+	home* arr;
+	arr = new home[func.quantity];
+
+	func.setting(gh, arr);
+
+	delete[] arr;
+	return 0;
+	}
