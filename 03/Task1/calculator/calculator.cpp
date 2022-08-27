0f8886c7c01549d4a77acc7c9be10dc2a0c0fe25
Anna1112333 <anna_1112333@mail.ru>
2022-08-28 01:18:36 +0500

    Исправление

--- 03/Task1/calculator/calculator.cpp
+++ 03/Task1/calculator/calculator.cpp
@@ -4,69 +4,71 @@ using namespace std;
 
 class Calculator
 {
-public:    // всеобщий
-    double num1;
-    double num2;
-    double add()
-    {
-        return num1 + num2;
-    }
-    double multiply()
-    {
-        return num1 * num2;
-    }
-    double subtract_1_2()
-    {
-        return num1 - num2;
-    }
-    double subtract_2_1()
-    {
-        return num2 - num1;
-    }
-    double divide_1_2()
-    {
-        return num1 / num2;
-    }
-    double divide_2_1()
-    {
-        return num2 / num1;
-    }
-    bool checking()
-    {
-        return (set_num1(num1) && set_num2(num2));
-    }
 private:
-        bool set_num1(double num1)
-    {
-        return static_cast<bool>(num1);
-    }
-    bool set_num2(double num2)
-    {
-        return static_cast<bool>(num2);
-    }
+	double num1;
+	double num2;
+
+public:
+	bool checking = true;
+	double add()
+	{
+		return num1 + num2;
+	}
+	double multiply()
+	{
+		return num1 * num2;
+	}
+	double subtract_1_2()
+	{
+		return num1 - num2;
+	}
+	double subtract_2_1()
+	{
+		return num2 - num1;
+	}
+	double divide_1_2()
+	{
+		return num1 / num2;
+	}
+	double divide_2_1()
+	{
+		return num2 / num1;
+	}
+	
+	void set_num1()
+	{
+		cin >> num1;
+		if (num1 == 0) checking = false;
+	}
+	void set_num2()
+	{
+		cin >> num2;
+		if (num2 == 0) checking = false;
+	}
+	
 };
 
 int main()
 {
-    Calculator paar;
-    setlocale(LC_ALL, "rus");
-    do {
-        cout << "Введите num1: ";
-        cin >> paar.num1;
-        cout << "Введите num2: ";
-        cin >> paar.num2;
-        if (paar.checking())
-        {
-            cout << "num1 + num2 = " << paar.add() << "\n";
-            cout << "num1 - num2 = " << paar.subtract_1_2() << "\n";
-            cout << "num2 - num1 = " << paar.subtract_2_1() << "\n";
-            cout << "num1 * num2 = " << paar.multiply() << "\n";
-            cout << "num1 / num2 = " << paar.divide_1_2() << "\n";
-            cout << "num2 / num1 = " << paar.divide_2_1() << "\n";
+	Calculator paar;
+	setlocale(LC_ALL, "rus");
+	do {
+		cout << "Введите num1: ";
+		paar.set_num1();
+		cout << "Введите num2: ";
+		paar.set_num2();
+		if (paar.checking)
+		{
+			cout << "num1 + num2 = " << paar.add() << "\n";
+			cout << "num1 - num2 = " << paar.subtract_1_2() << "\n";
+			cout << "num2 - num1 = " << paar.subtract_2_1() << "\n";
+			cout << "num1 * num2 = " << paar.multiply() << "\n";
+			cout << "num1 / num2 = " << paar.divide_1_2() << "\n";
+			cout << "num2 / num1 = " << paar.divide_2_1() << "\n";
 
-        }
-        else cout << "Неверный ввод!\n";
-    } while (true);
-    return 0;
+		}
+		else cout << "Неверный ввод!\n";
+	} while (true);
+	return 0;
 
 }
--- 03/Task2/counter/counter.cpp
+++ 03/Task2/counter/counter.cpp
@@ -5,66 +5,74 @@ using namespace std;
 class counter
 {
 public:
-    
-    void cases(char sign)
-    {
 
-        int type[] = { '-', '+', '=', -27 };
-        if ((int)sign == type[0])
-            subtract();
-        else if ((int)sign == type[1])
-            add();
-        else if ((int)sign == type[2])
-            equal();
-        else if ((int)sign == type[3])
-            exit();
-        else cout << "\nНеверный ввод!\n";
-    }
+	void cases(char sign)
+	{
+
+		int type[] = { '-', '+', '=', 'x'};
+		if ((int)sign == type[0])
+			subtract();
+		else if ((int)sign == type[1])
+			add();
+		else if ((int)sign == type[2])
+			equal();
+		else if ((int)sign == type[3])
+			exit();
+		else cout << "\nНеверный ввод!\n";
+	}
 
 private:
-    int number;
-    int close = 0;
-    void add()
-    {
-        number++;
-     }
-   void subtract()
-    {
-        number--;   
-    }
-   void exit()
-   {
-       cout << "До свидания.";
-      close++;
-   }
-   void equal()
-   {
-       cout << number<<"\n";
-   }
+	int number;
+	int close = 0;
+	void add()
+	{
+		number++;
+	}
+	void subtract()
+	{
+		number--;
+	}
+	void exit()
+	{
+		cout << "До свидания.";
+		close++;
+	}
+	void equal()
+	{
+		cout << number << "\n";
+	}
 public:
-   void be_done()
-   {
-       char any_sign;
-       cin >> number;
-       do {
-           cout << "Введите команду ('+', '-', '=' или 'x'):";
-           cin >> any_sign;
-           cases(any_sign);
-           if (close == 1) break;
-       } while (true);
-   }
+	void be_done()
+	{
+		char any_sign;
+		cin >> number;
+		do {
+			cout << "Введите команду ('+', '-', '=' или 'x'):";
+			cin >> any_sign;
+			cases(any_sign);
+		} while (close != 1);
+	}
 };
 
 int main()
 {
-    char agreement[5];
-    counter number1;
-    setlocale(LC_ALL, "rus");
-    cout << "Вы хотите указать начальное значение счётчика? Введите да или нет:";
-    cin >>agreement;
-    if (strlen(agreement)==3)
-    { cout << "До свидания"; return 0; }
-     cout << "Введите начальное значение счётчика: ";
-     number1.be_done();
-    return 0;
+	string no = "no";
+	string yes = "yes";
+	string agreement;
+	counter number1;
+	setlocale(LC_ALL, "rus");
+	cout << "Вы хотите указать начальное значение счётчика? Введите yes или no: ";
+	cin >> agreement;
+	cout << agreement<<endl;
+	if (!agreement.compare(no))
+	{
+		cout << "До свидания"; return 0;
+	}
+	else if (!agreement.compare(yes))
+	{
+		cout << "Введите начальное значение счётчика: ";
+		number1.be_done();
+	}
+	else cout << "Неверный ввод. Перезапустите прорамму.";
+	return 0;
 }
