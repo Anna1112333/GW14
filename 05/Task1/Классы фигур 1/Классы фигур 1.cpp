05e13d2739cd4b1bb601679d2f67604840650d54
Anna1112333 <anna_1112333@mail.ru>
2022-09-09 20:12:45 +0500

    task1

---
+++ 05/Task1/Классы фигур 1/Классы фигур 1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+class figure 
+{
+public:
+    int sides_count=0;
+    string fig_name = "Фигура";
+    virtual  void get_sides_count() { sides_count = sides;
+    cout << fig_name << ": " << sides_count;
+    }
+
+    figure(){ sides = 0;
+    string fig_name = "Фигура";
+    };
+    int sides;
+};
+
+class triangle : public figure
+{public:
+    triangle() {
+        fig_name = "Треугольник";
+       sides = 3;
+        cout << sides_count;
+    };
+    void get_sides_count() override {
+        sides_count = sides;
+        cout <<"\n" << fig_name << ": " << sides_count;
+    };
+ };
+
+class quadrangle : public figure
+{public:
+    quadrangle()
+    {
+        sides = 4;
+        fig_name = "Четырёхугольник";
+    };
+    void get_sides_count() override {
+        sides_count = sides;
+        cout << "\n" << fig_name << ": " << sides_count;
+    };
+};
+int main()
+{
+    figure a;
+    figure* p = &a;
+    triangle vg;
+    triangle* b = &vg;
+    quadrangle c;
+    quadrangle* chet = &c;
+    setlocale(LC_ALL, "rus");
+    cout << "Колличество сторон: \n";
+    p->get_sides_count();
+    b->get_sides_count();
+    chet->get_sides_count();
+   
+}
+
