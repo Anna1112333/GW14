c4bcb63877732264cd43f7cb67902cb0a1b10e9b
Anna1112333 <anna_1112333@mail.ru>
2022-11-29 10:38:40 +0500

    1 задание передоно в конструктор родителя

--- 05/Task1/Классы фигур 1/Классы фигур 1.cpp
+++ 05/Task1/Классы фигур 1/Классы фигур 1.cpp
@@ -4,58 +4,52 @@ using namespace std;
 
 class figure 
 {
-private:
-    string name = "Фигура";
+protected:
+    string name = "Фигура ";
     int sides=0;
+    int si =1;
     //присвоение
-    figure(string name, int sides)
+    figure(string &name, int &sides)
     {
         this->name = name;
         this->sides = sides;
-    };
+     };
 public:
-    figure() : figure(name, sides)
-    {
-        print1();
-    };
-public:
+    figure(){};
     void print1()
     {
-        cout << name << "сторон: " << sides << endl;
+        si = 1;
+        cout <<"Фигура: " << name << "сторон: " << sides << endl;
     }
 };
 
 class triangle : public figure
-{public:
-        string name = "Треугольник";
+{protected:
+        string name = "Треугольник ";
         int sides = 3;
-    triangle():figure()
-    {
-        
-    };
+public:
+    triangle() { triangle(this->name, this->sides); }
+    triangle(string name, int sides):figure(name,sides)
+    {  print1(); };
  };
 
 class quadrangle : public figure
-{public:
-        int sides = 4;
-        string name = "четырёхугольник";
-        quadrangle():figure()
-    {
-       
-    };
+{protected:
    
-};
+        int sides = 4;
+        string name = "Четырёхугольник ";
+public:
+    quadrangle() { quadrangle(this->name, this->sides); }
+       quadrangle( string name,int sides): figure(name,sides)
+    { print1(); };
+ };
 
 int main()
-{
-    triangle vg;
-    triangle* b = &vg;
-    quadrangle c;
-    quadrangle* chet = &c;
+{  
     setlocale(LC_ALL, "rus");
-    cout << "Колличество сторон: \n";
-    figure aa; aa.print1();
-   
-    //c.print1();
-    //chet->print1();
-}
+    cout << "Фигуры и колличество сторон: \n";
+    figure aa;
+   aa.print1();
+    triangle vg;
+    quadrangle fg;
+  }
