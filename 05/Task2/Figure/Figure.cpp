080bd9e11cd7ee20493d45913ca6106aef2bfebb
Anna1112333 <anna_1112333@mail.ru>
2022-11-30 21:55:47 +0500

    Задание 2

--- 05/Task2/Figure/Figure.cpp
+++ 05/Task2/Figure/Figure.cpp
@@ -3,140 +3,139 @@
 using namespace std;
 class figure
 {
-public:
-void print_info(int i)
-{
-    cout << fig_name << ":\n";
-    cout << "Cтороны:" <<"a=" << a << " b=" << b << " c=" << c;
-    if (i == 4) cout <<" d="<< d; 
-    cout << endl;
-    cout << "Углы: A="<<A<<" B="<<B<<" C="<<C;
-    if (i == 4) cout <<" D=" << D; 
-    cout << endl<<endl;
-}
-int sides;
 protected:
-    string fig_name = "Фигура";
-    int A=0, B=0, C=0, D=0;
-    int a=0, b=0, c=0, d=0;
-    figure(int i) { sides = i; }
- };
+	int A = 0, B = 0, C = 0, D = 0;
+	int a = 0, b = 0, c = 0, d = 0;
+private:
+	string name = "Фигура";
+public:
+	void print_info(int A, int B, int C)
+	{
+		cout << name << ":\n";
+		cout << "Cтороны:" << "a=" << a << " b=" << b << " c=" << c << endl;
+		cout << "Углы: A=" << A << " B=" << B << " C=" << C << endl << endl;
+	}
+	void print_info(int A, int B, int C, int D)
+	{
+		cout << name << ":\n";
+		cout << "Cтороны:" << "a=" << a << " b=" << b << " c=" << c << " d=" << d << endl;
+		cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << endl << endl;
+	}
+
+protected:
+	figure(string name1, string name)
+	{
+		this->name = name1;    
+	}
+};
 
 class triangle : public figure
 {
 public:
-    triangle(): figure(3)
-    {        
-        fig_name = "Треугольник";
-    }
+	triangle(string name1) : figure(name1, "Треугольник")
+	{}
 };
 //равнобедренный
-class isosceles_triangle : public triangle 
-{public:
-    isosceles_triangle(): triangle()
-    {
-        a = 10;  b = 10;  c = 15;
-        A = 41;  B = 41;  C = 98;
-        fig_name = "Равнобедренный треугольник";
-    }
+class isosceles_triangle : public triangle
+{
+public:
+	
+	isosceles_triangle() :triangle("Равнобедренный треугольник")
+	{
+		a = 10;  b = 10;  c = 15;
+		A = 41;  B = 41;  C = 98;
+		print_info(A, B, C);
+	}
 };
 //равносторонний
 class equilateral_triangle : public triangle
 {
 public:
-    equilateral_triangle() : triangle()
-    {
-        a = 10;  b = 10;  c = 10;
-        A = 60;  B = 60;  C = 60;
-        fig_name = "Равноcторонний треугольник";
-    }
+	equilateral_triangle() : triangle("Равноcторонний треугольник")
+	{
+		a = 10;  b = 10;  c = 10;
+		A = 60;  B = 60;  C = 60;
+		print_info(A, B, C);
+	}
 };
 
 //прямоугольный
 class right_triangle : public triangle
 {
 public:
-    right_triangle() : triangle()
-    {
-        a = 14;  b = 14;  c = 20;
-        A = 45;  B = 45;  C = 90;
-        fig_name = "Прямоугольный треугольник";
-    }
+	right_triangle() : triangle("Прямоугольный треугольник")
+	{
+		a = 14;  b = 14;  c = 20;
+		A = 45;  B = 45;  C = 90;
+		print_info(A, B, C);
+	}
 };
-
+//четырёхугольник
 class quadrangle : public figure
 {
 public:
-    quadrangle() : figure(4)
-    {
-        fig_name = "Четырёхугольник";
-    }
+	quadrangle(string name1) : figure(name1, "Четырёхугольник")
+	{}
 };
 
 //прямоугольник
 class rectangle : public quadrangle
 {
 public:
-    rectangle() : quadrangle()
-    {
-        a = 14;  b = 20;  c = 14; d = 20;
-        A = B = C = D= 90;
-        fig_name = "Прямоугольник";
-    }
+	rectangle() : quadrangle("Прямоугольник")
+	{
+		a = 14;  b = 20;  c = 14; d = 20;
+		A = B = C = D = 90;
+		print_info(A, B, C, D);
+	}
 };
 //квадрат
-class square : public rectangle
+class square : public quadrangle
 {
 public:
-    square() : rectangle()
-    {
-        a = b = c = d = 20;
-        fig_name = "Квадрат";
-    }
+	square() : quadrangle("Квадрат")
+	{
+		a = b = c = d = 20;
+		A = B = C = D = 90;
+		print_info(A, B, C, D);
+	}
 };
 //параллелограмм
 class parallelogram : public quadrangle
 {
 public:
-    parallelogram() : quadrangle()
-    {
-        a = 14;  b = 20;  c = 14; d = 20;
-        A = 45;  B = 135;  C = 45; D = 135;
-        fig_name = "Параллелограмм";
-    }
+	parallelogram() : quadrangle("Параллелограмм")
+	{
+		a = 14;  b = 20;  c = 14; d = 20;
+		A = 45;  B = 135;  C = 45; D = 135;
+		print_info(A, B, C, D);
+	}
 };
 
 //ромб
 class rhomb : public quadrangle
 {
 public:
-    rhomb() : quadrangle()
-    {
-        a = 20;  b = 20;  c = 20; d = 20;
-        A = 45;  B = 135;  C = 45; D = 135;
-        fig_name = "Ромб";
-    }
+	rhomb() : quadrangle("Ромб")
+	{
+		a = 20;  b = 20;  c = 20; d = 20;
+		A = 45;  B = 135;  C = 45; D = 135;
+		print_info(A, B, C, D);
+	}
 };
 int main()
-{     
-    figure q1 = isosceles_triangle();
-    figure q2 = equilateral_triangle();
-    figure q3 = right_triangle();
+{
+	setlocale(LC_ALL, "rus");
+	
+	figure q1 = isosceles_triangle();
+	figure q2 = equilateral_triangle();
+	figure q3 = right_triangle();
 
-    figure j1 = rectangle();
-    figure j2 = square();
-    figure j3 = parallelogram();
-    figure j4 = rhomb();
-    setlocale(LC_ALL, "rus");
- 
-   q1.print_info(q1.sides);
-   q2.print_info(q2.sides);
-   q3.print_info(q3.sides);
+	figure j1 = rectangle();
+	figure j2 = square();
+	figure j3 = parallelogram();
+	figure j4 = rhomb();
 
-   j1.print_info(j1.sides);
-   j2.print_info(j2.sides);
-   j3.print_info(j3.sides);
-   j4.print_info(j4.sides);
-   return 0;
+	return 0;
 }
 
